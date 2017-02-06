9833100466718b864c41154d34956d1e06afe86e
Anthony Tokatly <atokatly884@gmail.com>
2017-02-06 13:40:05 -0800

    Create initial workspace

---
+++ Ripe/Ripe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ripe
+//
+//  Created by Anthony Tokatly on 2/6/17.
+//  Copyright © 2017 tokatlys-tantilizers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
